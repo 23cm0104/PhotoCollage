bd8b3996fe4c66f3e1c828af23a21d77a15ae78a
23cm0104 <23cm0104@jec.ac.jp>
2024-06-28 20:14:37 +0900

    Initial Commit

---
+++ PhotoCollage/PhotoCollageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollageApp.swift
+//  PhotoCollage
+//
+//  Created by cmStudent on 2024/06/28.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCollageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
